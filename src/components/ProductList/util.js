0eb03cdf18c39f94a22ad5e7def27fc5f38fee26
fus <elbartas321@gmail.com>
2020-12-03 19:02:58 +0200

    more testingz

--- src/components/ProductList/util.js
+++ src/components/ProductList/util.js
@@ -3,44 +3,44 @@ import find from "lodash/find";
 export function formProdList(data, oneRow) {
   const triplData = [];
 
-  let prodSet = [];
-  for (let i = 0; i < data.length; i++) {
-    const prod = data[i];
+  // let prodSet = [];
+  // for (let i = 0; i < data.length; i++) {
+  //   const prod = data[i];
 
-    if (!oneRow && i !== 0 && Number.isInteger(i / 3)) {
-      triplData.push(prodSet);
-      prodSet = [];
-    }
+  //   if (!oneRow && i !== 0 && Number.isInteger(i / 3)) {
+  //     triplData.push(prodSet);
+  //     prodSet = [];
+  //   }
 
-    let itemsAvailable = false;
+  //   let itemsAvailable = false;
 
-    for (let j = 0; j < prod.sizes.length; j++) {
-      const size = prod.sizes[j];
+  //   for (let j = 0; j < prod.sizes.length; j++) {
+  //     const size = prod.sizes[j];
 
-      if (parseInt(size.amount, 10) > 0) {
-        itemsAvailable = true;
-        break;
-      }
-    }
+  //     if (parseInt(size.amount, 10) > 0) {
+  //       itemsAvailable = true;
+  //       break;
+  //     }
+  //   }
 
-    const adjProd = {
-      id: prod._id,
-      title: prod.title,
-      price: prod.price,
-      discPrice: prod.discPrice,
-      primaryPic: find(prod.imgData, "primary")?.imgUrl,
-      secondaryPic: find(prod.imgData, "secondary")?.imgUrl,
-      disabled: !itemsAvailable,
-    };
+  //   const adjProd = {
+  //     id: prod._id,
+  //     title: prod.title,
+  //     price: prod.price,
+  //     discPrice: prod.discPrice,
+  //     primaryPic: find(prod.imgData, "primary")?.imgUrl,
+  //     secondaryPic: find(prod.imgData, "secondary")?.imgUrl,
+  //     disabled: !itemsAvailable,
+  //   };
 
-    prodSet.push(adjProd);
+  //   prodSet.push(adjProd);
 
-    if (oneRow && oneRow - 1 === i) {
-      break;
-    }
-  }
+  //   if (oneRow && oneRow - 1 === i) {
+  //     break;
+  //   }
+  // }
 
-  triplData.push(prodSet);
+  // triplData.push(prodSet);
 
   return triplData;
 }
