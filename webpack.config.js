78085400aa83496300e316924ab0e88197996891
fus <elbartas321@gmail.com>
2020-12-03 17:53:00 +0200

    testing with node modules NOT excluded

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        // exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         },
