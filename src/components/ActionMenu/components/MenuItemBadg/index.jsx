084392b093206a2a92298c85498052f3b6881ef9
fus <elbartas321@gmail.com>
2020-12-06 12:31:55 +0200

    another final adjustment

--- src/components/ActionMenu/components/MenuItemBadg/index.jsx
+++ src/components/ActionMenu/components/MenuItemBadg/index.jsx
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { toggleMenu } from "redux_store/general/actions";
 /* styles */
 import { MenuItemStyle } from "../MenuItem/style";
@@ -6,10 +7,16 @@ import { StyledMenBadge } from "./style";
 
 export const MenuItemBadg = () => {
   const cartItems = useSelector((state) => state.cartItems);
+  const history = useHistory();
   const dispatch = useDispatch();
 
   return (
-    <MenuItemStyle to="/krepselis" onClick={() => dispatch(toggleMenu())}>
+    <MenuItemStyle
+      onClick={() => {
+        dispatch(toggleMenu());
+        history.push("/krepselis");
+      }}
+    >
       <StyledMenBadge
         invisible={!cartItems.length}
         badgeContent={cartItems.length}
